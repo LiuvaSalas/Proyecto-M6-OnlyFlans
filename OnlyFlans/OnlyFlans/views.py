187f116aad2e80a9bbadaa42ac6d15932eda4bdf
LiuvaSalas <liuvasalas01@gmail.com>
2024-10-25 16:32:12 -0300

    -index.html: se genero un for para las card, desde view para mostrar el contenido de un listado de postres mas su respectiva imagen referecial. -views.html: se genero una nueva version de la funcion index.

--- OnlyFlans/OnlyFlans/views.py
+++ OnlyFlans/OnlyFlans/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
 #INDEX - Landing Page
-def index(request):
-    return render(request, "index.html", {})
+#def index(request):
+#    return render(request, "index.html", {})
 
 def about(request):
     return render(request, "about.html", {})
 
 def welcome(request):
     return render(request, "welcome.html", {})
+
+def index(request):
+    postres = [
+        {"nombre": "Flan de Chocolate", "descripcion": "Flan con sabor a chocolate negro", "ruta": "assets/img/productos/flan-chocolate-negro.jpg"},
+        {"nombre": "Flan de Vainilla", "descripcion": "Flan sabor Vainilla con topping de Chocolate Blanco", "ruta": "assets/img/productos/flan-toping-chocolate-blanco.avif"},
+        {"nombre": "Fran Tradicional", "descripcion": "Flan Tradicional", "ruta": "assets/img/productos/flan-tradicional.jpg"}
+    ]
+    contexto = {"postres": postres}
+    return render(request, "index.html", contexto)
