3f0f6f6cfc6e6cbf60e70c13143417ee7dcd6607
LiuvaSalas <liuvasalas01@gmail.com>
2024-11-05 19:56:45 -0300

    Se genero el cambio de la base de datos en settings.

--- OnlyFlans/OnlyFlans/settings.py
+++ OnlyFlans/OnlyFlans/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "OnlyFlans.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "onlyflans",
+        "USER": "postgres",
+        "PASSWORD": "liuvis01",
+        "HOST": "localhost",
+        "DATABASE_PORT": "5432",
     }
 }
 
